2de7f0fbafbab83e0df111e6368875ef9eecaf5a
Benjamin Schölkmann <bschlkmann@gmail.com>
2021-05-11 11:37:15 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Benjamin Schölkmann on 11.05.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
